54b577c87ca16ea4ad9fa2de852e6f6eedac30d7
Murad Qocayev <muradqocayevi20@gmail.com>
2023-02-03 16:32:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Ededden özünün rəqəmləri cəmini çıxmaq tək ədəd alınana kimi
+
+// let num = prompt("Enter number");
+// let total = 0;
+// for (let i = 0; i < num.length; i++) {
+//   total += +num[i];
+// }
+// function number(num) {
+//   while (true) {
+//     num -= total;
+//     if (num % 2 !== 0) {
+//       return num;
+//     }
+//   }
+// }
+// console.log(number(num));
+
+
+// Input ile daxil olunan ədədin tərsinin bölənlərini göstərin
+
+let num = '273'
+let numRev = num.split('').reverse().join('')
